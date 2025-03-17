2fe650ebb0e567692512f0eb649e31c04ea9dfdd
kozue.miyazawa <hatsusora@yahoo.co.jp>
2025-03-17 14:28:26 +0900

    TangibleAsset Book Computer 修正

--- src/practiceQuestions/Book.java
+++ src/practiceQuestions/Book.java
@@ -1,32 +1,15 @@
 package practiceQuestions;
 
-public class Book extends TangibleAsset{
-    String name;
-    int price;
-    String color;
+public class Book extends TangibleAsset {
     String isbn;
 
     //コンストラクタ
     public Book(String name, int price, String color, String isbn) {
-        this.name = name;
-        this.price = price;
-        this.color = color;
+        super(name, price, color);
         this.isbn = isbn;
     }
 
     //メソッド
-    public String getName() {
-        return this.name;
-    }
-
-    public int getPrice() {
-        return this.price;
-    }
-
-    public String getColor() {
-        return this.color;
-    }
-
     public String getIsbn() {
         return this.isbn;
     }
--- src/practiceQuestions/Computer.java
+++ src/practiceQuestions/Computer.java
@@ -1,32 +1,15 @@
 package practiceQuestions;
 
-public class Computer extends TangibleAsset{
-    String name;
-    int price;
-    String color;
+public class Computer extends TangibleAsset {
     String makerName;
 
     //コンストラクタ
     public Computer(String name, int price, String color, String makerName) {
-        this.name = name;
-        this.price = price;
-        this.color = color;
+        super(name, price, color);
         this.makerName = makerName;
     }
 
     //メソッド
-    public String getName() {
-        return this.name;
-    }
-
-    public int getPrice() {
-        return this.price;
-    }
-
-    public String getColor() {
-        return this.color;
-    }
-
     public String getMakerName() {
         return this.makerName;
     }
